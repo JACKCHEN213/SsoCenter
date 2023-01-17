210328c351dabef814a5e55b40e3163cda87d7f6
陈超 <1366294101@qq.com>
2023-01-17 17:28:22 +0800

    feat: 添加数据库文件

---
+++ extend/sso_center.sql
@@ -0,0 +1,62 @@
+/*
+ Date: 17/01/2023 17:25:55
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP DATABASE IF EXISTS sso_center;
+CREATE DATABASE IF NOT EXISTS sso_center;
+
+use sso_center;
+
+-- ----------------------------
+-- Table structure for sc_login_history
+-- ----------------------------
+DROP TABLE IF EXISTS `sc_login_history`;
+CREATE TABLE `sc_login_history`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `login_time` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '登录记录表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for sc_site
+-- ----------------------------
+DROP TABLE IF EXISTS `sc_site`;
+CREATE TABLE `sc_site`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `request_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `redirect_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `is_use` tinyint NULL DEFAULT 1,
+  `is_del` tinyint NULL DEFAULT 0,
+  `public_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `app_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for sc_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sc_user`;
+CREATE TABLE `sc_user`  (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of sc_user
+-- ----------------------------
+INSERT INTO `sc_user` VALUES (1, 'admin', 'c3284d0f94606de1fd2af172aba15bf3', '2022-12-21 10:51:45', '2023-01-09 16:43:33');
+
+SET FOREIGN_KEY_CHECKS = 1;
